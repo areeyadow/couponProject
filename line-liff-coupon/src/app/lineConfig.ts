65363ca114795a21af0bafeca8d65f7c950ec08b
areeyadow <161796781+areeyadow@users.noreply.github.com>
2025-06-14 22:59:58 +0700

    fix

--- line-liff-coupon/src/app/lineConfig.ts
+++ line-liff-coupon/src/app/lineConfig.ts
@@ -1,5 +1,6 @@
 // LINE API Configuration
 export const LINE_CONFIG = {
-  CHANNEL_ACCESS_TOKEN: process.env.LINE_CHANNEL_ACCESS_TOKEN || "",
+  CHANNEL_ACCESS_TOKEN:
+    "VAB9CsOtzecT8MbNdorYTx4MiKPJu/BEQtElqz7ZTbzSSsvNYwVxTab+0DkKJoUimHcMQdi4wvXv6MXXwRl/FPnmysaX84+Sdb109RuJUsj3B3t/pIO/1rjSipvjiBAh8rJ/NgBmzkD+n5y1e3Jj3wdB04t89/1O/w1cDnyilFU=",
   MESSAGING_API_URL: "https://api.line.me/v2/bot/message/push",
 };
